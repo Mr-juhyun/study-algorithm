aed7435751fde3ff8869d93e8bced0745a2ee00f
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-08-15 11:54:33 +0900

    Update 디스크 컨트롤러.py

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,8 +1,9 @@
 def solution(jobs):
-    process_time = min(jobs)[0]
+    process_time = 0
+    result_time = 0
+    length = len(jobs)
     jobs = sorted(jobs,key=lambda x:x[1])
-    time = []
-    for _ in range(len(jobs)):
+    for _ in range(length):
         x = 0
         for disk in jobs:
             if disk[0] <= process_time:
@@ -12,10 +13,8 @@ def solution(jobs):
         if x == 0:
             disk = min(jobs)
             process_time = sum(disk)
-        time.append(process_time - disk[0])   
+            
+        result_time += process_time - disk[0]
         jobs.remove(disk)
-        xxxxx=11111111111
-        yyyyyy=111111111
-        for i in range(xxxxx):
-            print(i)
-    return sum(time)//len(time)
+        
+    return result_time//length
