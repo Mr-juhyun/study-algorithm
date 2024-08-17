89802e99c635c198afad3490a326200e0c0d8c97
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-08-17 14:00:36 +0900

    [level 3] Title: 야근 지수, Time: 217.88 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,17 +1,21 @@
 from collections import Counter
-import heapq
 def solution(n, works):
     if sum(works) <= n:
         return 0
     answer = 0
-    works = list(map(lambda x:-x,works))
-    heapq.heapify(works)
-    for _ in range(n):
-        max_work = heapq.heappop(works)
-        max_work += 1
-        heapq.heappush(works,max_work)
-        
     works = Counter(works)
+    while n >0:
+        max_work = max(works)
+        target = works.pop(max_work) 
+        if target > n:
+            works[max_work] = target-n
+            works[max_work-1] += n
+            break
+        else:
+            n -= target
+            works[max_work-1] += target
     for work in works:
         answer += work**2 * works[work]
     return answer
+
+
