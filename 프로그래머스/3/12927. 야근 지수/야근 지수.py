835398fe91195422eaeb1774e1d0bf9f4017709b
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-08-19 20:32:17 +0900

    [level 3] Title: 야근 지수, Time: 3.76 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,25 +1,26 @@
 from collections import Counter
-
 def solution(n, works):
     if sum(works) <= n:
         return 0
     
-    work_counts = Counter(works)
-    max_work = max(work_counts)
-    
-    while n > 0 and max_work > 0:
-        count = work_counts[max_work]
-        
-
-        if count > n:
-            work_counts[max_work] -= n
-            work_counts[max_work - 1] += n
+    answer = 0
+    max_work = max(works := Counter(works))
+    while n >0:
+        target = works.pop(max_work) 
+        if target > n:
+            works[max_work] = target-n
+            works[max_work-1] += n
             break
+        else:
+            n -= target
+            works[max_work-1] += target
+            max_work -= 1
+            
+    for work in works:
+        answer += work**2 * works[work]
+    return answer
 
-        work_counts[max_work - 1] += count
-        n -= count
-        del work_counts[max_work] 
-        max_work -= 1 
-    
-    return sum(work ** 2 * count for work, count in work_counts.items())
+
+
+
 
