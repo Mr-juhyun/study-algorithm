1c8c65252085b41a99344a2aeb5d481a9e5c8ec6
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-09-04 23:41:31 +0900

    [level 2] Title: 양궁대회, Time: 1.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,36 +1,30 @@
 def solution(n, info):
-    
-    def differ(ryan, apeach):
-        diff = 0
-        for idx in range(len(info)-1):
-            score = 10 - idx
-            if ryan[idx] > apeach[idx]:
-                diff += score
-            elif ryan[idx] < apeach[idx]:
-                diff -= score
-        return diff
-    
-    ryans_score = [[]]
-    for apeach_shot in info:
+    ryan_score = [[n,0]]
+    max_difference = 0
+    for idx in range(len(info)):
+        apeach_shot = info[idx]
         temp = []
-        for ryan_shot in ryans_score:
-            temp.append(ryan_shot + [0])
-            if apeach_shot + 1 <= n:
-                temp.append(ryan_shot + [apeach_shot + 1])
-        ryans_score = temp
-
-    ryans = []
-    for ryan in ryans_score:
-        if sum(ryan) <= n:
-            ryan[-1] += (n - sum(ryan))
-            ryan.append(differ(ryan,info))
-            ryans.append(ryan)
-
-    max_ryan = max(ryans, key=lambda x: x[-1])
-    if max_ryan[-1] <= 0:
+        score = 10-idx
+        for ryan_shot in ryan_score:
+            diff = ryan_shot.pop()
+            if idx == 10:
+                if diff >= max_difference: 
+                    temp.append(ryan_shot[1:] + [ryan_shot[0]]+[diff])
+                    max_difference = diff
+            else:
+                if apeach_shot > 0:
+                    diff -= score
+                    temp.append(ryan_shot +[0]+[diff])
+                    diff += score
+                else:
+                    temp.append(ryan_shot +[0]+[diff])
+                if apeach_shot + 1 <= ryan_shot[0]:
+                    diff += score
+                    ryan_shot[0] -= apeach_shot +1 
+                    temp.append(ryan_shot + [apeach_shot+1]+[diff])
+        
+        ryan_score = temp
+    ryan_score.sort(key=lambda x: x[::-1],reverse=True)
+    if not ryan_score or ryan_score[0].pop() <= 0:
         return [-1]
-    
-    answer = list(filter(lambda x:x[-1] == max_ryan[-1],ryans))
-    answer.sort(key=lambda x: x[::-1],reverse=True)
-    
-    return answer[0][:-1]
+    return ryan_score[0]
