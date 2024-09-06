d29077f2be839583fcbb4354759ef2f5c7483855
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-09-06 16:34:41 +0900

    [level 3] Title: 불량 사용자, Time: 300.80 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -3,13 +3,12 @@ from itertools import permutations,combinations
 
 def solution(user_id, banned_id):
     banned_id = [i.replace('*', '.') for i in banned_id]
-
     answer= set()
     all_user = permutations(user_id,len(banned_id))
     for user in all_user:
         cnt = 0
         for i in range(len(banned_id)):
-            if len(user[i]) == len(banned_id[i]) and re.match(banned_id[i],user[i]) is not None:
+            if len(user[i]) == len(banned_id[i]) and re.match(banned_id[i],user[i]):
                 cnt +=1
         if cnt == len(banned_id):    
             answer.add(tuple(sorted(user)))
