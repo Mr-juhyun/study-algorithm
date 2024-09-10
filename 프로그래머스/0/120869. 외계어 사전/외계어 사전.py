d1bc0a4b5c87725cc2edb4a93cfbd407bea8c069
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-09-10 22:21:45 +0900

    [level 0] Title: 외계어 사전, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,9 +1,7 @@
-from itertools import permutations
 def solution(spell, dic):
-    spell = permutations(spell,len(spell))
-    for per_spell in spell:    
-        for word in dic:
-            target = str(''.join(per_spell))
-            if word == target:
-                return 1
+    for word in dic:
+        print(word)
+        if sorted(word) == sorted(spell):
+            print(sorted(word),sorted(spell))
+            return 1
     return 2
