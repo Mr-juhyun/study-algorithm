13a304bfbd26af192621298cc63c3bd8902c5f9f
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2024-09-18 14:26:58 +0900

    [level 3] Title: 정수 삼각형, Time: 38.81 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,6 @@
+def solution(triangle):
+    for row in range(len(triangle) - 2, -1, -1):
+        for col in range(len(triangle[row])):
+            triangle[row][col] += max(triangle[row + 1][col], triangle[row + 1][col + 1])
+    
+    return triangle[0][0]
