b858201fe3f7cb9631381b75144534851ebb1cea
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2025-02-15 23:52:38 +0900

    [level 1] Title: 유연근무제, Time: 2.21 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -0,0 +1,29 @@
+def solution(schedules, timelogs, startday):
+    answer = 0
+
+    for idx,time in enumerate(schedules):
+        day = startday
+        h , m = divmod(time,100)
+        m += 10
+        if m >= 60:
+            h += 1
+            m -= 60
+        h = str(h)
+        m = str(m)
+        if len(m) == 1:
+            m = "0" + m
+            
+        go_to_work = int(h + m)
+        check = True
+        
+        for timelog in timelogs[idx]:
+            if day <= 5:
+                if timelog > go_to_work:
+                    check = False
+                    break
+                    
+            day += 1
+            if day > 7: day = 1
+                
+        if check: answer += 1
+    return answer
