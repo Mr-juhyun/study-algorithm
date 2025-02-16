8b0440b54534d9bc7c0f6b8ea9000b039cfcd126
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2025-02-16 17:36:32 +0900

    [level 2] Title: 할인 행사, Time: 224.47 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,14 @@
+from collections import Counter
+def solution(want, number, discount):
+    s = 0
+    dis_lst = discount[s:s+10]
+    answer = 0
+    want = {want[i]:number[i] for i in range(len(want))}
+    
+    while len(dis_lst) == 10:
+        if want == dict(Counter(dis_lst)):
+            answer += 1
+        s += 1
+        dis_lst = discount[s:s+10]
+        
+    return answer
