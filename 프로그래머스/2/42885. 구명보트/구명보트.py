3b0a5d263382fb829c4951dde1d13748a1ec5bbb
김주현 <159969391+Mr-juhyun@users.noreply.github.com>
2026-02-28 19:48:39 +0900

    [level 2] Title: 구명보트, Time: 10.63 ms, Memory: 9.65 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,11 @@
+def solution(people, limit):
+    people.sort()
+    l = 0
+    r = len(people) - 1
+    boat = 0
+    while l <= r:
+        if people[l] + people[r] <= limit:
+            l +=1
+        r -= 1
+        boat += 1
+    return boat
